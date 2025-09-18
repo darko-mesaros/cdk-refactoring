f0cdfab49597873057c0b8c88a3a478d1aadee8f
Darko Mesaros <darko.subotica@gmail.com>
2025-09-18 12:14:40 -0700

    init

--- lib/mono_stack-stack.ts
+++ lib/mono_stack-stack.ts
@@ -1,16 +1,64 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as apigw from 'aws-cdk-lib/aws-apigatewayv2';
+import { RustFunction } from 'cargo-lambda-cdk';
+import { HttpLambdaIntegration } from 'aws-cdk-lib/aws-apigatewayv2-integrations';
+import { HttpMethod } from 'aws-cdk-lib/aws-events';
 
 export class MonoStackStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Lambda function
+    const readerLambda = new RustFunction(this, 'readerLambda', {
+      manifestPath: './lambda/reader_lambda/Cargo.toml',
+      timeout: cdk.Duration.seconds(30),
+      memorySize: 128,
+      architecture: lambda.Architecture.ARM_64,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'MonoStackQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    //API Gateway
+    const api = new apigw.HttpApi(this, 'Api',{
+      apiName: 'product-api',
+    });
+
+    const readerInteg = new HttpLambdaIntegration('readerInteg', readerLambda);
+    api.addRoutes({
+      path: '/read',
+      methods: [HttpMethod.GET],
+      integration: readerInteg
+    });
+
+    // Data - PRODUCT
+    const productDb = new dynamodb.TableV2(this, 'productDb', {
+      partitionKey:  {
+        name: 'ProductId',
+        type: dynamodb.AttributeType.STRING
+      }
+    });
+
+    productDb.grantReadData(readerLambda);
+
+    // Data - USERS
+    const userDb = new dynamodb.TableV2(this, 'userDb', {
+      partitionKey:  {
+        name: 'UserId',
+        type: dynamodb.AttributeType.STRING
+      }
+    });
+
+    userDb.grantReadData(readerLambda);
+
+    // Data - ORDERS
+    const orderDb = new dynamodb.TableV2(this, 'orderDb', {
+      partitionKey:  {
+        name: 'OrderId',
+        type: dynamodb.AttributeType.STRING
+      }
+    });
+
+    orderDb.grantReadData(readerLambda);
   }
 }
